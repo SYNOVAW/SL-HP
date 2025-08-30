7adc2858e01bbf77090db33fdd1083e1c7e3ae36
Jayne Yu <jayneyu@JaynenoMacBook-Pro.local>
2025-08-30 20:52:05 +0900

    Simplify middleware configuration to fix invocation errors

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-  // A list of all locales that are supported
   locales: ['en', 'zh', 'ja'],
-  
-  // Used when no locale matches
-  defaultLocale: 'en',
-  
-  // Always use locale prefix
-  localePrefix: 'always'
+  defaultLocale: 'en'
 });
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ['/', '/(zh|ja|en)/:path*']
 };
